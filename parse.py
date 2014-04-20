f6ed855ed312dca9007e45b934b440bc2c89c52c
Aiko Lin <aiko0403@gmail.com>
2014-04-20 12:16:44 -0700

    successfully parse and print

--- parse.py
+++ parse.py
@@ -3,37 +3,93 @@ import fileinput
 import sys
 
 r = re.compile(' - ')
-i=0
-j=0
+
+# inilialize variable
+Title = ""
+DOI = ""
+ISSN = ""
+Volumn = ""
+Issue = ""
+Start_page = ""
+End_page = ""
+N_of_page = ""
+N_of_author = ""
+N_of_keyword = ""
+Abstract = ""
+
+
+# fout = open(sys.stdout, 'a')
 
 for line in fileinput.input():    
-	i=i+1
-	print i
-	j=0
 	elements = r.split(line)
-	#elements = line.replace('-',' ').split()
-	# elements = r.split(line)
 
-	# for value in elements:
-	# 	j=j+1
-	# 	value = value.rstrip()
-	# 	value = value.split('\r\t\n')
-	# 	print("|" + str(j) + value[0]  + "|")
-	if i==2:
+	if len(elements) <2:
 		continue
+	
 	elements[0] = elements[0].rstrip().split('\r\t\n')
 	elements[1] = elements[1].rstrip().split('\r\t\n')
 
-	if elements[0]=='TI':
-		print('Title '+elements[1])
-	else if elements[0]=='DO':
-		print('DOI '+element)
-		pass
+	if ''.join(elements[0])=='TY':
+		# inilialize variable
+		Title = ""
+		DOI = ""
+		ISSN = ""
+		Volumn = ""
+		Issue = ""
+		Start_page = ""
+		End_page = ""
+		N_of_page = ""
+		N_of_author = ""
+		N_of_keyword = ""
+		Abstract = ""
+	
+	if ''.join( elements[0] ) == 'TI':
+		print 'Title ' + ''.join( elements[1] )
+		Title = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'DO':
+		print 'DOI ' + ''.join( elements[1] )
+		DOI = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'SN':
+		print 'ISSN ' + ''.join( elements[1] )
+		ISSN = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'VO':
+		print 'Volumn ' + ''.join( elements[1] )
+		Volumn = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'IS':
+		print 'Issue ' + ''.join( elements[1] )
+		Issue = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'SP':
+		print 'Start_page ' + ''.join( elements[1] )
+		Start_page = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'EP':
+		print 'End_page ' + ''.join( elements[1] )
+		End_page = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'DO':
+		print 'DOI ' + ''.join( elements[1] )
+		DOI = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'SN':
+		print 'ISSN ' + ''.join( elements[1] )
+		ISSN = ''.join( elements[1] )
+	elif ''.join( elements[0] ) == 'VO':
+		print 'Volumn ' + ''.join( elements[1] )
+		Volumn = ''.join( elements[1] )
 
 
 fileinput.close()
 
 
+
+# ISSN
+# Volumn
+# Issue
+# Start page
+# End page
+# # of page (calculate)
+# Author array
+# # of author (calculate)
+# Keyword array
+# # of keyword (calculate)
+# Abstract
 
 
 
